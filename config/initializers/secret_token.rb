4b34a4772974c706b157ce7b723753299a51af96
Roviry <ross@roviry.com.au>
2013-06-15 16:31:53 +1000

    Start the TennisPro project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tennispro::Application.config.secret_token = '84ee8db2329116ae6b1ae6482d423ae12b3b03c4ad08e1be57c42101d80f3f8e6566d0d0cde71051418dc3eb3e8c1e6ae60a30620f3650ce5166e18df176395f'
