b17f94e0327fda9633947daaf90d8e7d8fdd9d71
Roviry <ross@roviry.com.au>
2013-06-16 15:18:52 +1000

    First pass of static pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Tennispro::Application.routes.draw do
 
   get "static_pages/contact"
 
+  get "static_pages/about"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+    describe "Home page" do
+        before { visit '/static_pages/home' }
+
+        it "should have the <h1> tag 'Home'" do
+            page.should have_selector('h1', :text => 'Home')
+        end
+
+        it "should have the title 'Home'" do
+            page.should have_selector('title', :text => 'Home')
+        end
+    end
+
+    describe "Help page" do
+        before { visit '/static_pages/help' }
+
+        it "should have the <h1> tag 'Help'" do
+            page.should have_selector('h1', :text => 'Help')
+        end
+
+        it "should have title 'Help'" do
+            page.should have_selector('title', :text => 'Help')
+        end
+
+    end
+
+    describe "About page" do
+        before { visit '/static_pages/about' }
+
+        it "should have the <h1> tag 'About'" do
+            page.should have_selector('h1', :text => 'About')
+        end
+
+        it "should have title 'About'" do
+            page.should have_selector('title', :text => 'About')
+        end
+    end
+
+    describe "Contact page" do
+        before { visit '/static_pages/contact' }
+
+        it "Should have the <h1> tag 'Contact'" do
+            page.should have_selector('h1', :text => 'Contact Us')
+        end
+
+        it "Should jave the title 'Contact'" do
+            page.should have_selector('title', :text => 'Contact Us')
+        end
+    end
+end
